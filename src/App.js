3c51c4f631e2c9f8dcc56fbecbfcd96f8b505d3e
mercykorkoressel <mercyessel1005@gmail.com>
2024-04-16 17:31:16 +0000

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-
-import './App.css';
+import React from 'react';
+import LecturerLogin from './pages/lec-login';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Homepage from './pages/homepage';
+import LecturerSignup from './pages/lec-signup';
+import StudentDash from './pages/studash';
+import StudentLogin from './pages/stu-login';
+import LecturerDash from './pages/lecdash';
+import StudentSignup from './pages/stu-signup';
+import LecHome from './pages/lec-home';
 
 function App() {
   return (
-    <div className="App">
+
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/lechome' element={<LecHome/>} />
+          <Route path='/leclogin' element={<LecturerLogin />} />
+          <Route path='/lecsignup' element={<LecturerSignup />} />
+          <Route path='/stulogin' element={<StudentLogin/>} />
+          <Route path='/stusignup' element={<StudentSignup/>} />
+          <Route path='/studash' element={<StudentDash/>} />
+          <Route path='/lecdash' element={<LecturerDash/>} />
+        </Routes>
+      </BrowserRouter>
       
     </div>
+    
   );
 }
 
---
+++ src/pages/lec-home.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import stu1 from "../Assets/stu5.jpg";
+import coa from"../Assets/COA.png"
+
+export default function LecHome() {
+   
+  return (
+    <div className="h-screen overflow-hidden flex">
+      <div className="w-1/2 relative">
+        {/* Image with gradient overlay */}
+        <div className="absolute inset-0 z-10 flex justify-center items-center">
+          <img src={stu1} className="object-cover w-full h-full" alt="Student" />
+        </div>
+        {/* Gradient overlay */}
+        <div className="absolute inset-0 z-20">
+          <div className="w-full h-full bg-gradient-to-l from-[#9563E1] to-transparent"></div>
+        </div>
+      </div>
+      <div className="w-1/2 bg-[#9563E1] flex flex-col justify-center items-center">
+        <img src={coa}></img>
+        <h1 className='text-white pl-10 text-4xl  font-bold'>THE ARMY UNIVERSITY             
+        COLLEGE</h1><sm className="text-white pb-10">student/staff Portal</sm>
+        <div className="flex gap-10 justify-center items-center  pb-10">
+          <div>
+            <label className="inline-flex items-center" htmlFor="indigoCheckBox">
+              <input id="indigoCheckBox" type="checkbox" className="w-6 h-6 rounded-full border-2 border-indigo-700 focus:ring-indigo-400" />
+              <a href="/" class=" ml-2">Student</a>
+            </label>
+          </div>
+          <div>
+            <label className="inline-flex items-center" htmlFor="lecturerCheckBox">
+              <input id="lecturerCheckBox" type="checkbox" className="w-6 h-6 rounded-full border-2 border-indigo-700 focus:ring-indigo-400" />
+              <span className="ml-2">Lecturer</span>
+            </label>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="rounded-md shadow">
+            <a href="/lecsignup" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+              SIGN UP
+            </a>
+          </div>
+          <div className="mt-3 sm:mt-0 sm:ml-3">
+            <a href="/leclogin" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+              LOG IN
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
